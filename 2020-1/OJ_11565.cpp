8fdd73e1dcd97921ed396856d1908c57b7be8492
Akyom <42448128+Akyom@users.noreply.github.com>
2021-09-01 13:53:35 -0400

    Update 2021-2

--- 2020-1/OJ_11565.cpp
+++ 2020-1/OJ_11565.cpp
@@ -13,17 +13,13 @@ int main(){
 		bool q=false;
 		cin >> a >> b >> c;//lee entrada
 		int l=(int)sqrt(c-2);// l=limite de x
-		//cout << "l: " << l << "\n";
 		for(x=-l;x<=l;++x){//x desde el limite inferior hasta el limite superior (dado por la ecuacion 3)
 			if(x && !(b%x)){
-				//cout << "x: "<< x << "\n";
 				int ax=abs(x);
 				for(y=-(b/ax);y<=(b/ax);++y){//y desde el limite inferior hasta el limite superior (dado por la ecuacion 2)
 					if(y&&y!=x){
 						if(!(b%(x*y))){
-							//cout << "y: " << y << "\n";
 							z=a-x-y;//z dado por la ecuacion 1 ya teniendo x e y
-							//cout << "z: " << z << "\n";
 							if(z!=y && z!=x && x*y*z==b && (x*x)+(y*y)+(z*z)==c){ //comprueba las ecuaciones 2 y 3
 								cout << x << " " << y << " " << z << "\n"; // si encuentra una solucion la imprime y pasa al siguiente caso
 								q=true;
